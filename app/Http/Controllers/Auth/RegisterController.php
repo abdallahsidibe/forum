4bb5abeb1442f9a3671e4dd55857ef4e2ed12af6
abdallah monastir <sidibeabdallah01@gmail.com>
2021-06-23 21:51:14 +0100

    minor change

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -28,7 +28,7 @@ class RegisterController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = '/home';
+    protected $redirectTo = '/';/**il va fait le choix */
 
     /**
      * Create a new controller instance.
